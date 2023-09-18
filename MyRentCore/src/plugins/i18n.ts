d0697e2950229496db46e4cb8ba5142e220ff9da
Roman <roman.spelina@actaware.com>
2023-09-18 18:28:50 +0200

    i18n pl and en translations files

--- MyRentCore/src/plugins/i18n.ts
+++ MyRentCore/src/plugins/i18n.ts
@@ -1,6 +1,6 @@
 import {createI18n} from "vue-i18n";
-import pl from "@/lang/pl";
-import en from '@/lang/en'
+import pl from "@/lang/pl"
+import en from "@/lang/en"
 
 
 export const i18n = createI18n({
